2f4e22839d3529ac41f5569117cd42160f84b135
itzel <itzelalwarafii@gmail.com>
2024-10-09 19:19:12 -0700

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter  basename={window.location.pathname || ''}>
+    <BrowserRouter  basename="/MyPortafolio">
     <App />
     </BrowserRouter>
   </StrictMode>,
