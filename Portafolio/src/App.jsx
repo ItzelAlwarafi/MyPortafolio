bcbd227fd8255897d61f5e28a3ee2e1d5378f5d1
itzel <itzelalwarafii@gmail.com>
2024-10-09 18:14:27 -0700

    setup

---
+++ Portafolio/src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import './App.css'
+
+function App() {
+  <Routes>
+  <Route path="/" element={<LandingPage />} />
+  </Routes>
+  
+}
+
+export default App
---
+++ Portafolio/src/components/footer.jsx
@@ -0,0 +1,11 @@
+
+export default function footer (){
+    return(
+        <div className="footer-container">
+            <h1 className="footer-text">© 2024 ITZEL ALWARAFI. All rights reserved. </h1>
+            
+
+        </div>
+        
+    )
+}
