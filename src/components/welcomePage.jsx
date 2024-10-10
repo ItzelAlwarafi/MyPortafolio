0d08401cf08af43021fb79df98b3cd9a94bc768e
itzel <itzelalwarafii@gmail.com>
2024-10-09 21:12:41 -0700

    changes

--- src/components/welcomePage.jsx
+++ src/components/welcomePage.jsx
@@ -1,22 +1,16 @@
+import welcomeImage from '../assets/welcomeImage.jpg'
 
-
-
-
-export default function welcomePage (){
-    return(
-
-<div className="welcome-page-container">
-    <div className="intro-message">
-        
-    <h2 className="regular-font-group">  Hello, my name is</h2>
-<h1 className="bold-font-group">ITZEL ALWARAFI</h1>
-<h2 className="regular-font-group">I'm a Full Stack Software Developer</h2>
-
-    </div>
-
-</div>
-       
-
-
+export default function WelcomePage() {
+    return (
+        <div
+            className="welcome-page-container"
+            style={{ backgroundImage: `url(${welcomeImage})` }} 
+        >
+            <div className="intro-message">
+                <h2 className="regular-font-group">Hello, my name is</h2>
+                <h1 className="bold-font-group">ITZEL ALWARAFI</h1>
+                <h2 className="regular-font-group">I'm a Full Stack Software Developer</h2>
+            </div>
+        </div>
     )
 }
